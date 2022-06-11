a3f3a9552502d6da1ae8c186e71144fc82ee6fc3
takehiro <takehiro-r133@hotmail.co.jp>
2022-06-11 20:28:39 +0900

    add UserDetalModal

--- src/components/organisms/user/UserCard.tsx
+++ src/components/organisms/user/UserCard.tsx
@@ -3,13 +3,15 @@ import { Box, Image, Stack, Text } from "@chakra-ui/react"
 import { memo } from "react"
 
 type Props = {
+  id: number
   imageUrl: string
   userName: string
   fullName: string
+  onClick: (id: number) => void
 }
 
 export const UserCard: React.FC<Props> = memo((props) => {
-  const { imageUrl, userName, fullName } = props
+  const { id, imageUrl, userName, fullName, onClick } = props
   return (
     <Box
       w='260px'
@@ -19,6 +21,7 @@ export const UserCard: React.FC<Props> = memo((props) => {
       shadow='md'
       p={4}
       _hover={{ cursor: "pointer", opacity: 0.8 }}
+      onClick={() => onClick(id)}
     >
       <Stack textAlign='center'>
         <Image
--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,13 +1,30 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { Center, Spinner, Wrap, WrapItem } from "@chakra-ui/react"
-import { memo, useEffect } from "react"
+import {
+  Center,
+  Spinner,
+  useDisclosure,
+  Wrap,
+  WrapItem,
+} from "@chakra-ui/react"
+import { memo, useCallback, useEffect } from "react"
 import { useAllUsers } from "../../hooks/useAllUsers"
+import { useSelectUser } from "../../hooks/useSelectUser"
 import { UserCard } from "../organisms/user/UserCard"
+import { UserDetailModal } from "../organisms/user/UserDetailModal"
 
 export const UserManagement: React.FC = memo(() => {
+  const { isOpen, onOpen, onClose } = useDisclosure()
   const { getUsers, users, loading } = useAllUsers()
-
+  const { onSelectUser, selectedUser } = useSelectUser()
   useEffect(() => getUsers(), [])
+
+  const onClickUser = useCallback(
+    (id: number) => {
+      onSelectUser({ id, users, onOpen })
+    },
+    [users, onSelectUser, onOpen]
+  )
+
   return (
     <>
       {loading ? (
@@ -15,18 +32,21 @@ export const UserManagement: React.FC = memo(() => {
           <Spinner />
         </Center>
       ) : (
-        <Wrap p={{ base: 4, md: 10 }} justify="space-around">
+        <Wrap p={{ base: 4, md: 10 }} justify='space-around'>
           {users.map((user) => (
             <WrapItem key={user.id}>
               <UserCard
+                id={user.id}
                 imageUrl='https:source.unsplash.com/random'
                 userName={user.username}
                 fullName={user.name}
+                onClick={onClickUser}
               />
             </WrapItem>
           ))}
         </Wrap>
       )}
+      <UserDetailModal user={selectedUser} isOpen={isOpen} onClose={onClose} />
     </>
   )
 })
